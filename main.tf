df8cf42ce5e61d2bd486b7ac9b3693b950480699
Stiven Avila <fabian.avila@iteraprocess.com>
2025-08-08 11:29:37 -0500

    cambio

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ data "aws_caller_identity" "current" {}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.0.8"
+  version = "~> 21.0"
 
   cluster_name                   = var.cluster_name
   cluster_version                = var.kubernetes_version
