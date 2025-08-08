7861f1f6086d3716c69f7c1c854b1455c5c67ac1
Stiven Avila <fabian.avila@iteraprocess.com>
2025-08-08 11:00:05 -0500

    name cluster

--- variables.tf
+++ variables.tf
@@ -1,17 +1,19 @@
 variable "cluster_name" {
   description = "Name of the EKS cluster and associated resources"
   type        = string
+  default     = "k8s-demo-spacelift"
 }
 
 variable "region" {
   description = "AWS region in which to deploy the EKS cluster"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "kubernetes_version" {
   description = "Kubernetes version to use for the EKS control plane"
   type        = string
-  default     = "1.29"
+  default     = "1.32"
 }
 
 variable "environment" {
